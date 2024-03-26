3d7f0cb6e568b55b312c79be75ebfaffeb69e786
HHHHHHHHP <792650995@qq.com>
2024-03-26 20:26:50 +0800

    初始化补充：点击初始界面的不同按钮转跳到不同的深层界面

--- lib/exam/exam.dart
+++ lib/exam/exam.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'exam-screen-1.dart';
+import 'exam-screen-2.dart';
+
 class Exam extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Page Exam')),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Page Exam'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailedExamScreen1()),
+                );
+              },
+              child: Text('前往 detailed exam screen 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailedExamScreen2()),
+                );
+              },
+              child: Text('前往 detailed exam screen 2'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
