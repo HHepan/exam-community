bae55b1f44205d5ff30dd302e99482b07332933b
HHHHHHHHP <792650995@qq.com>
2024-03-29 10:44:01 +0800

    发布帖子界面

---
+++ lib/community/add-blog.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AddBlog extends StatelessWidget {
+  int _currentLines = 1; // 初始行数为 1
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('发布帖子'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                minLines: 5, // 设置最小行数为 5
+                maxLines: null, // 不限制最大行数
+                decoration: InputDecoration(
+                    hintText: '请输入内容',
+                    border: OutlineInputBorder()
+                ),
+              ),
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 300, // 设置父容器的宽度
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 按钮点击事件处理逻辑
+                  },
+                  child: Text('确认发布'),
+                ),
+              ),
+              Container(
+                width: 300, // 设置父容器的宽度
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 按钮点击事件处理逻辑
+                  },
+                  child: Text('取消'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
