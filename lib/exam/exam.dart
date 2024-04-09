718af0ecb167fc2cf3851e0754ffe80ae8b8bf47
Wangs <Wangs131@outlook.com>
2024-04-09 17:22:49 +0800

    draw the exam page

--- lib/exam/exam.dart
+++ lib/exam/exam.dart
@@ -1,37 +1,61 @@
 import 'package:flutter/material.dart';
 
-import 'exam-screen-1.dart';
-import 'exam-screen-2.dart';
-
 class Exam extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Page Exam'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DetailedExamScreen1()),
-                );
-              },
-              child: Text('前往 detailed exam screen 1'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DetailedExamScreen2()),
-                );
-              },
-              child: Text('前往 detailed exam screen 2'),
-            ),
-          ],
-        ),
+      body: GridView.count(
+        crossAxisCount: 1,
+        padding: EdgeInsets.all(1.0),
+        childAspectRatio: 2.0 / 4.0,
+        children: <Widget>[
+          Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 1.0 / 1.0,
+                  child: Image.asset('images/OIP.png'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('我的考试'),
+                    style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(Size(275, 100)),
+                      alignment: Alignment.center,
+                      elevation: MaterialStateProperty.all(4.0),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        )
+                      )
+                    ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('随机考试'),
+                    style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(Size(275, 100)),
+                      alignment: Alignment.center,
+                      elevation: MaterialStateProperty.all(4.0),
+                      shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      )
+                  ),
+                )
+              ],
+            )
+          ),
+        ],
       ),
     );
   }
