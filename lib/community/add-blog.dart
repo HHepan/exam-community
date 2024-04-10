469a6e517cc68f698e674c8bf909ae925970fca1
HHHHHHHHP <792650995@qq.com>
2024-04-11 01:24:22 +0800

    delete two cancel buttons

--- lib/community/add-blog.dart
+++ lib/community/add-blog.dart
@@ -34,17 +34,7 @@ class AddBlog extends StatelessWidget {
                   },
                   child: Text('确认发布'),
                 ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                width: 360, // 设置父容器的宽度
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('取消'),
-                ),
-              ),
+              )
             ],
           ),
         ],
